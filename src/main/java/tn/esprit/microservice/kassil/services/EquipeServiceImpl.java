ad3e78558f007279ea0d65ad64e18bfbfb33794c
assilbelhaj <100833883+assilbelhaj@users.noreply.github.com>
2025-04-04 15:26:55 +0100

    Add Equipe microservice with Docker setup

--- src/main/java/tn/esprit/microservice/kassil/services/EquipeServiceImpl.java
+++ src/main/java/tn/esprit/microservice/kassil/services/EquipeServiceImpl.java
@@ -3,80 +3,49 @@ package tn.esprit.microservice.kassil.services;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import tn.esprit.microservice.kassil.entities.Contrat;
 import tn.esprit.microservice.kassil.entities.Equipe;
-import tn.esprit.microservice.kassil.entities.Etudiant;
 import tn.esprit.microservice.kassil.entities.Niveau;
 import tn.esprit.microservice.kassil.repositories.EquipeRepository;
 
-
-import java.util.Date;
 import java.util.List;
-import java.util.Set;
 
 @Slf4j
 @AllArgsConstructor
 @Service
-public class EquipeServiceImpl implements IEquipeService{
-    EquipeRepository equipeRepository;
+public class EquipeServiceImpl implements IEquipeService {
 
+    private final EquipeRepository equipeRepository;
 
-    public List<Equipe> retrieveAllEquipes(){
-        return  (List<Equipe>) equipeRepository.findAll();
-    }
-    public Equipe addEquipe(Equipe e){
-        return (equipeRepository.save(e));
+    public List<Equipe> retrieveAllEquipes() {
+        return (List<Equipe>) equipeRepository.findAll();
     }
 
-    public  void deleteEquipe(Integer idEquipe){
-        Equipe e=retrieveEquipe(idEquipe);
-        equipeRepository.delete(e);
+    public Equipe addEquipe(Equipe e) {
+        return equipeRepository.save(e);
     }
 
-    public Equipe retrieveEquipe(Integer equipeId){
-        return equipeRepository.findById(equipeId).get();
+    public void deleteEquipe(Integer idEquipe) {
+        equipeRepository.deleteById(idEquipe);
     }
 
-    public Equipe updateEquipe(Equipe e){
-        return (	equipeRepository.save(e));
+    public Equipe retrieveEquipe(Integer equipeId) {
+        return equipeRepository.findById(equipeId).orElse(null);
     }
 
-    public void evoluerEquipes(){
+    public Equipe updateEquipe(Equipe e) {
+        return equipeRepository.save(e);
+    }
+
+    public void evoluerEquipes() {
         List<Equipe> equipes = (List<Equipe>) equipeRepository.findAll();
         for (Equipe equipe : equipes) {
-            if ((equipe.getNiveau().equals(Niveau.JUNIOR)) || (equipe.getNiveau().equals(Niveau.SENIOR))) {
-                List<Etudiant> etudiants = (List<Etudiant>) equipe.getEtudiants();
-                Integer nbEtudiantsAvecContratsActifs=0;
-                for (Etudiant etudiant : etudiants) {
-                    Set<Contrat> contrats = etudiant.getContrats();
-                    //Set<Contrat> contratsActifs=null;
-                    for (Contrat contrat : contrats) {
-                        Date dateSysteme = new Date();
-                        long difference_In_Time = dateSysteme.getTime() - contrat.getDateFinContrat().getTime();
-                        long difference_In_Years = (difference_In_Time / (1000l * 60 * 60 * 24 * 365));
-                        if ((contrat.getArchive() == false) && (difference_In_Years > 1)) {
-                            //	contratsActifs.add(contrat);
-                            nbEtudiantsAvecContratsActifs++;
-                            break;
-                        }
-                        if (nbEtudiantsAvecContratsActifs >= 3) break;
-                    }
-                }
-                if (nbEtudiantsAvecContratsActifs >= 3){
-                    if (equipe.getNiveau().equals(Niveau.JUNIOR)){
-                        equipe.setNiveau(Niveau.SENIOR);
-                        equipeRepository.save(equipe);
-                        break;
-                    }
-                    if (equipe.getNiveau().equals(Niveau.SENIOR)){
-                        equipe.setNiveau(Niveau.EXPERT);
-                        equipeRepository.save(equipe);
-                        break;
-                    }
-                }
+            if (equipe.getNiveau() == Niveau.JUNIOR) {
+                equipe.setNiveau(Niveau.SENIOR);
+                equipeRepository.save(equipe);
+            } else if (equipe.getNiveau() == Niveau.SENIOR) {
+                equipe.setNiveau(Niveau.EXPERT);
+                equipeRepository.save(equipe);
             }
-
         }
-
     }
 }
