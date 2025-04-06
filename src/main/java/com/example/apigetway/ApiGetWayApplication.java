e242f2d414ad282359f1c97abe16487804150c77
oussamaawledsalem <oussama.awledsalem@esprit.tn>
2025-04-06 13:19:02 +0100

    still not working

--- src/main/java/com/example/apigetway/ApiGetWayApplication.java
+++ src/main/java/com/example/apigetway/ApiGetWayApplication.java
@@ -14,26 +14,34 @@ public class ApiGetWayApplication {
 	}
 
 	@Bean
-	public RouteLocator gatewayRoutes (RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route("Contrat", r -> r.path("/Contrat/**")
-						.uri("lb://CONTRAT"))//candidat:8056
-				.route("universite", r -> r.path("/universite/**")
-						.uri("lb://UNIVERSITE"))//job:8081
+		public RouteLocator gatewayRoutes (RouteLocatorBuilder builder) {
+			return builder.routes()
+					.route("Contrat", r -> r.path("/Contrat/**")
+							.uri("lb://CONTRAT"))//candidat:8056
+					.route("universite", r -> r.path("/universite/**")
+							.uri("lb://UNIVERSITE"))//job:8081
 
-				.route("Ressource", r -> r.path("/Ressource/**")
-						.uri("lb://RESSOURCE"))//job:8081
+					.route("Ressource", r -> r.path("/Ressource/**")
+							.uri("lb://RESSOURCE"))//job:8081
 
-				.route("departementMicroService", r -> r.path("/departementMicroService/**")
-						.uri("lb://DEPARTEMENTMICROSERVICE"))
-				.route("formation", r -> r.path("/formation/**")
-						.uri("lb://formation"))
-				.route("equipe", r -> r.path("/equipe/**")
-						.uri("lb://equipe"))
+					.route("departementMicroService", r -> r.path("/departementMicroService/**")
+							.uri("lb://DEPARTEMENTMICROSERVICE"))
+					.route("formation", r -> r.path("/formation/**")
+							.filters(f -> f
+									.rewritePath("/formation/(?<segment>.*)", "/${segment}")
+									// OR use .stripPrefix(1) instead of rewritePath
+									.removeRequestHeader("Origin")
+									.dedupeResponseHeader("Access-Control-Allow-Origin", "RETAIN_FIRST")
+									.dedupeResponseHeader("Access-Control-Allow-Credentials", "RETAIN_FIRST")
+							)
+							.uri("lb://FORMATION")
+					)
+					.route("equipe", r -> r.path("/equipe/**")
+							.uri("lb://equipe"))
 
 
 
 
-				.build();
-	}
+					.build();
+		}
 }
