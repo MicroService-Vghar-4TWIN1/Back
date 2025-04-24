a88ee239b936dfd9a8a6ecec244ee44bb87f1a6a
ChihebDabebi <dabebi.chiheb@esprit.tn>
2025-04-24 13:03:19 +0100

    Dockerfile and fix eureka

--- server.js
+++ server.js
@@ -1,35 +1,48 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
+const cors = require("cors"); // Add this line
+
 const { Eureka } = require('eureka-js-client');
+require("dotenv").config();
 
 const mongoose = require("mongoose");
-const configDb = require("./config/db.json");
+console.log("MongoDB URI:", process.env.MONGODB_URI); // Debugging
 
 const financialAidRouter = require('./routers/financialAid');
+// CORS Configuration
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
 
 const eurekaClient = new Eureka({
-    instance: {
-      app: 'FINANCIALAID', 
-      instanceId: 'financial-aid-service-1',
-      hostName: 'localhost',
-      ipAddr: '127.0.0.1',
-      port: {
-        '$': 3000, 
-        '@enabled': true,
-      },
-      vipAddress: 'FINANCIALAID',
-      dataCenterInfo: {
-        '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-        name: 'MyOwn',
-      },
+  instance: {
+    app: 'FINANCIALAID',
+    instanceId: 'finance-service-1',
+    hostName: 'financeService',
+    ipAddr: 'financeService',
+    port: {
+      '$': 3000,
+      '@enabled': true,
     },
-    eureka: {
-      host: 'localhost', 
-      port: 8761,
-      servicePath: '/eureka/apps/',
+    vipAddress: 'FINANCIALAID',
+    dataCenterInfo: {
+      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+      name: 'MyOwn',
     },
-  });
+  },
+  eureka: {
+    host: 'eureka-server',
+    port: 8761,
+    servicePath: '/eureka/apps/',
+    maxRetries: 10,
+    requestRetryDelay: 2000,
+  },
+});
+
   
   eurekaClient.start((error) => {
     if (error) {
@@ -42,6 +55,7 @@ const eurekaClient = new Eureka({
 
 const app = express();
 const server = http.createServer(app);
+app.use(cors(corsOptions)); // Apply CORS middleware
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
@@ -53,8 +67,7 @@ app.use('/finance', financialAidRouter);
 // ceci est un exemple :
 
 
-mongoose
-  .connect(configDb.mongo.uri)
+mongoose.connect(process.env.MONGODB_URI|| 'mongodb://localhost:27017/financeDB')
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -63,6 +76,6 @@ mongoose
   });
 
 
-server.listen(3000, ()=>{
+server.listen(3000, '0.0.0.0', ()=>{
     console.log('server is running on http://localhost:3000');
 });
